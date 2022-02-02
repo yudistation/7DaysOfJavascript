e5ed6bad60e06c4c4f82f9a52f178ae035702633
yudistation <gabryudi@gmail.com>
2022-02-02 13:44:59 -0400

    Adicionado funções, dia 1 completo.

--- dia-1.js
+++ dia-1.js
@@ -5,20 +5,22 @@ let stringTrinta = '30'
 let numeroDez = 10
 let stringDez = '10'
 
-if (numeroUm == stringUm) {
-    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
-} else {
-    console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+verificarValor(numeroUm, stringUm)
+verificarTipo(numeroTrinta, stringTrinta)
+verificarValor(numeroDez, stringDez)
+
+function verificarValor(valor1, valor2) {
+    if (valor1 == valor2) {
+        console.log('As variáveis tem o mesmo valor')
+    } else {
+        console.log('As variáveis não tem o mesmo valor')
+    }
 }
 
-if (numeroTrinta === stringTrinta) {
-    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
-} else {
-    console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
-}
-
-if (numeroDez == stringDez) {
-    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
-} else {
-    console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+function verificarTipo(tipo1, tipo2) {
+    if (tipo1 === tipo2) {
+        console.log('As variáveis tem o mesmo tipo')
+    } else {
+        console.log('As variáveis são de tipos diferentes')
+    }
 }
